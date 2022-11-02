1d51661d7be66547604993239d92fd13e1138dc0
Viththiyakaran <battikaran6@gmail.com>
2022-11-02 21:47:50 +0530

    Add Db context with EF Core or SQLServer

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Work_with_Interface.Models;
+
+namespace Work_with_Interface.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base (options)
+        {
+
+        }
+
+        public DbSet<Actor> actors { get; set; }
+    }
+}
