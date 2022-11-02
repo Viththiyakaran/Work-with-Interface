6e21b57ee223371fb18130458fbd034d9e070f83
Viththiyakaran <battikaran6@gmail.com>
2022-11-02 21:54:40 +0530

    Add Actor controller

---
+++ Controllers/ActorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Work_with_Interface.Data;
+
+namespace Work_with_Interface.Controllers
+{
+    public class ActorController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public ActorController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var data = _db.actors.ToList();
+            return View(data);
+        }
+    }
+}
