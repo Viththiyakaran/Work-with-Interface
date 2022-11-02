ef6af2cc80c70115513a220637bc613781ce8d97
Viththiyakaran <battikaran6@gmail.com>
2022-11-02 23:26:00 +0530

    Add features in actor model

--- Models/Actor.cs
+++ Models/Actor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +9,12 @@ namespace Work_with_Interface.Models
 {
     public class Actor
     {
+        [Key]
+        [DisplayName("ID")]
         public int Id { get; set; }
-
+        [DisplayName("Full Name")]
         public string FullName { get; set; }
-
+        [DisplayName("Bio")]
         public string Bip { get; set; }
     }
 }
